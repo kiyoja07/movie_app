c575a7b9b3de3883aca7bd45c4862643f501d1cc
kioyoja07 <kiyoja07@gmail.com>
2019-02-16 01:56:11 +0900

    how to work reactjs

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// To render a React element into a root DOM node, pass both to ReactDOM.render():
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
